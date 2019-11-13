56944e6391a978a3072dda2ae0383d6def997918
Thomas Barrett <tsbarrett89@gmail.com>
2019-11-13 14:16:34 -0700

    added addnewfriend funcionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,11 +5,15 @@ import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import NavBar from './components/NavBar';
 import PrivateRoute from './components/PrivateRoute';
+import AddFriends from './components/AddFriends';
 
 function App() {
   return (
     <div>
+      <NavBar />
+
       <Route exact path='/' component={NavBar} />
+      <Route path='/addFriends' component={AddFriends} />
       <Route path='/login' component={Login} />
       <PrivateRoute path='/friendsList' component={FriendsList} />
     </div>
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = () => {
+    const [friends, setfriends] = useState('')
+    
+    useEffect (() => {
+        axiosWithAuth()
+            .get('/friends')
+            .then(response => {
+                console.log(response)
+                setfriends(response.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
             <h2>Friends!</h2>
+            <Link to='addFriends'><button>Add New Friends</button></Link>
+            {Array.from(friends).map((friend, index) => (
+                <div key={index}>
+                    <h3>{friend.name}</h3>
+                    <p>{friend.age}</p>
+                    <p>{friend.email}</p>
+                </div>
+            ))}
         </div>
     )
 }
